282afe84b90cf8c0be206eccb50f20d6bbd41daf
Naoki Orii <mrorii@gmail.com>
2013-04-13 16:21:56 -0400

    Add comments

--- Conll/conll/feature.py
+++ Conll/conll/feature.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
 
+'''
+Feature functions take as input a `Sentence` object and a int position.
+They should return a list of tuples, where the 1st element of the tuple
+contains the name of the feature, and the 2nd element of the tuple contains the
+value of the feature
+'''
+
+
 def is_first_word(sentence, position):
     return [('isFirstWord', int(position == 0))]
 
